66406a1c68db2740fb2a4b186b0eb39258187cf8
Rustywolf <dawsoking2@hotmail.com>
2015-05-09 21:28:03 +1000

    Create Bukkit Plugin for usage within plugins

---
+++ src/main/java/codes/rusty/chatapi/ChatAPIPlugin.java
@@ -0,0 +1,12 @@
+package codes.rusty.chatapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatAPIPlugin extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        this.getLogger().info("ChatAPI has initiated!");
+    }
+    
+}
